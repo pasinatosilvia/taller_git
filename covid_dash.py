33890611d9c8ea2a095a0811cc9c795f965ce6f5
Juan Maniglia <jamr1989@gmail.com>
2021-11-10 16:03:16 +0100

    trozo2

--- covid_dash.py
+++ covid_dash.py
@@ -1,15 +1,33 @@
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
-import pandas as pd
-import requests
-from datetime import datetime
-# https://towardsdatascience.com/building-a-real-time-dashboard-using-python-plotly-library-and-web-service-145f50d204f0
+def convertTime(t):
+    t = int(t)
+    return datetime.fromtimestamp(t)
 
-raw= requests.get("https://services1.arcgis.com/0MSEUqKaxRlEPj5g/arcgis/rest/services/Coronavirus_2019_nCoV_Cases/FeatureServer/1/query?where=1%3D1&outFields=*&outSR=4326&f=json")
-raw_json = raw.json()
-df = pd.DataFrame(raw_json["features"])
+df_final = df_final.dropna(subset=["Last_Update"])
+df_final["Province_State"].fillna(value="", inplace=True)
 
-data_list = df["attributes"].tolist()
-df_final = pd.DataFrame(data_list)
-df_final.set_index("OBJECTID")
-df_final = df_final[["Country_Region", "Province_State", "Lat", "Long_", "Confirmed", "Deaths", "Recovered", "Last_Update"]]
+df_final["Last_Update"]= df_final["Last_Update"]/1000
+df_final["Last_Update"] = df_final["Last_Update"].apply(convertTime)
+
+df_total = df_final.groupby("Country_Region", as_index=False).agg(
+    {
+        "Confirmed" : "sum",
+        "Deaths" : "sum",
+        "Recovered" : "sum"
+    }
+)
+
+total_confirmed = df_final["Confirmed"].sum()
+total_recovered = df_final["Recovered"].sum()
+total_deaths = df_final["Deaths"].sum()
+
+df_top10 = df_total.nlargest(10, "Confirmed")
+top10_countries_1 = df_top10["Country_Region"].tolist()
+top10_confirmed = df_top10["Confirmed"].tolist()
+
+df_top10 = df_total.nlargest(10, "Recovered")
+top10_countries_2 = df_top10["Country_Region"].tolist()
+top10_recovered = df_top10["Recovered"].tolist()
+
+df_top10 = df_total.nlargest(10, "Deaths")
+top10_countries_3 = df_top10["Country_Region"].tolist()
+top10_deaths = df_top10["Deaths"].tolist()
